4062e0e3344665ff3f20078a1ca14c0e42701d42
wasidc <wasid.chowdhury@icloud.com>
2025-09-22 14:06:29 -0400

    Change cars from Lexus

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -2,6 +2,6 @@ package com.pluralsight;
 
 public class Main{
     public static void main(String[] args) {
-        System.out.println("I love cars!");
+        System.out.println("I love Lexus!");
     }
 }
